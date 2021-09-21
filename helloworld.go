b07c95fa4a88802aeeb457afe21c1e7961a00829
aaronjiali@tencent.com <aaronjiali@tencent.com>
2021-09-21 21:29:44 +0800

    update port

--- helloworld.go
+++ helloworld.go
@@ -15,12 +15,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Print("Hello world sample started.")
-
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8081"
 	}
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
